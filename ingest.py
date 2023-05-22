ce6ef4a23c58152cad16603b2b4db7d29e545506
canlab-test-ai <test.ai@canlab.co>
2023-05-22 21:11:02 +0900

    Patch code for fixing errors.

--- ingest.py
+++ ingest.py
@@ -9,13 +9,17 @@ from langchain.vectorstores.faiss import FAISS
 
 def ingest_docs():
     """Get documents from web pages."""
-    loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
-    raw_documents = loader.load()
+    # loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
+    # raw_documents = loader.load()
+    with open("data-20230519/data_20230519.txt") as f:
+        state_of_the_union = f.read()
+
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
     )
-    documents = text_splitter.split_documents(raw_documents)
+    # documents = text_splitter.split_documents(raw_documents)
+    documents = text_splitter.create_documents([state_of_the_union])
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.from_documents(documents, embeddings)
 
